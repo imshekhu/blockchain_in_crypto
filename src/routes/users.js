6cde7d055030de35540b7c7f3ab6c3e2ce1921d1
Shourya SHarma <sharmashourya80@gmail.com>
2022-09-29 09:54:47 -0700

    authentication in place

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const router = express.Router();
-const { checkDuplicateEmail,  checkRolesExist} = require("../middlewares/verifySignup")
-const checkForEmail = require("../middlewares/verifyLogin")
+const { checkDuplicateEmail,  checkRolesExist} = require('../middlewares/verifySignup')
+const checkForEmail = require('../middlewares/verifyLogin')
+const auth = require('../middlewares/authToken')
 
 router.post('/login',checkForEmail, userController.loginUser);
 
 router.post('/signup', checkDuplicateEmail, userController.createUser);
 
-router.get('/:id',userController.getUser);
+router.get('/:id', auth.authenticateToken, userController.getUser);
 
 router.put('/update/:id', userController.updateUser);
 
